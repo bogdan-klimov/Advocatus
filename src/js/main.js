3cae4dc533d5727b83f5d36e966a8972311ab994
bogdan-klimov <bodyaklimov333@gmail.com>
2020-10-18 11:01:16 +0300

    firstCommit

--- src/js/main.js
+++ src/js/main.js
@@ -3,25 +3,6 @@ const slider = document.getElementById("our-clients-slider");
 const sliderEl = document.getElementsByClassName("our-clients-el");
 const sliderButton = document.getElementsByClassName("our-clients-list-item");
 
-const move = visibleWindow.offsetWidth;
-
-sliderButton[0].classList.add('active');
-
-for (let i = 0; i < sliderEl.length; i++) {
-    sliderButton[i].addEventListener("click", () => {
-        slider.style.left = (-move * i) + "px";
-        for (let i = 0; i < sliderEl.length; i++) {
-            sliderButton[i].className = sliderButton[i].className.replace("active", "");
-        };       
-        sliderButton[i].classList.add("active");
-    });
-};
-////////////////////////////////////////////////////////////////
-const questionItem = document.getElementsByClassName("privilege-questions-item");
-const questionDescription = document.getElementsByClassName("privilege-description");
-const questionItemText = document.getElementsByClassName("privilege-questions-item-text");
-const questionItemIcons = document.getElementsByClassName("icons");
-
 const addClass = (item, classItem) => {
     item.classList.add(classItem);
 }
@@ -30,19 +11,52 @@ const replaceClass = (item, classItem, newClassItem) => {
     item.className = item.className.replace(classItem, newClassItem);
 }
 
-addClass(questionDescription[0], "visible-item");
-addClass(questionItemText[0], "color-item");
-replaceClass(questionItemIcons[0], "icon-plus", "icon-minus")
+const move = visibleWindow.offsetWidth;
+
+sliderButton[0].classList.add('active');
+
+for (let i = 0; i < sliderEl.length; i++) {
+    sliderButton[i].addEventListener("click", () => {
+        slider.style.left = (-move * i) + "px";
+        for (let i = 0; i < sliderEl.length; i++) {
+
+            replaceClass(sliderButton[i], "active", "")
+
+        };
+        addClass(sliderButton[i], "active")
+    });
+};
+
+////////////////////////////////////////////////////////////////
+
+const questionItem = document.getElementsByClassName("privilege-questions-item");
+const questionDescription = document.getElementsByClassName("privilege-description");
+const questionItemText = document.getElementsByClassName("privilege-questions-item-text");
+const questionItemIcons = document.getElementsByClassName("icons");
+
+const activeElement = (idx) => {
+    addClass(questionDescription[idx], "active");
+    addClass(questionItemText[idx], "color-active");
+    replaceClass(questionItemIcons[idx], "icon-plus", "icon-minus");
+}
+
+activeElement(0);
 
 for (let i = 0; i < questionItem.length; i++) {
     questionItem[i].addEventListener("click", () => {
         for (let j = 0; j < questionItem.length; j++) {
-            replaceClass(questionItemText[j], "color-item", "");
-            replaceClass(questionDescription[j], "visible-item", "");
+            replaceClass(questionDescription[j], "active", "");
+            replaceClass(questionItemText[j], "color-active", "");
             replaceClass(questionItemIcons[j], "icon-minus", "icon-plus");
         };
-        addClass(questionDescription[i], "visible-item");
-        addClass(questionItemText[i], "color-item")
-        replaceClass(questionItemIcons[i], "icon-plus", "icon-minus") 
+        activeElement(i);
     });
 };
+
+////////////////////////////////////////////////////////////////
+const label = document.getElementById("check-id-label");
+
+label.addEventListener("click", () => {
+    label.classList.add("active-label");
+}); 
+////////////////////////////////////////////////////////////////////
